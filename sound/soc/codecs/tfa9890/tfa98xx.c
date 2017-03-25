ef77c063d2e596304538960d993ccf28c9476180
ahmedradaideh <ahmed.radaideh@gmail.com>
2017-03-25 13:08:18 +0200

    sound: silence logs spam
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- sound/soc/codecs/tfa9890/tfa98xx.c
+++ sound/soc/codecs/tfa9890/tfa98xx.c
@@ -74,7 +74,7 @@ EXPORT_SYMBOL_GPL(tfa_codec_np);
 static ssize_t tfa98xx_state_store(struct device *dev, struct device_attribute *attr,
 		const char *buf, size_t count)
 {
-    pr_err("%s",__func__);
+    pr_debug("%s",__func__);
 
 	return 0;
 }
@@ -467,7 +467,7 @@ static int tfa98xx_trigger(struct snd_pcm_substream *substream, int cmd,
 	//struct tfa98xx *tfa98xx = snd_soc_codec_get_drvdata(codec);
 	int ret = 0;
 
-	pr_err("trigger:%d(0:stop 1:start)\n", cmd);
+	pr_debug("trigger:%d(0:stop 1:start)\n", cmd);
 
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
@@ -703,7 +703,7 @@ void tfa98xx_play_stop(void)
 {
 	struct tfa98xx *tfa98xx = g_tfa98xx;
 
-    pr_err("tfa stop\n");
+    pr_debug("tfa stop\n");
 
     if(g_tfa98xx == NULL)
     {
