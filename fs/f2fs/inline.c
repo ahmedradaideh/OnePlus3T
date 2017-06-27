0d1f64b338a0a2072e8ced1e28c89556021a46d1
ahmedradaideh <ahmed.radaideh@gmail.com>
2017-06-28 01:34:00 +0300

    f2fs: Re-add OnePlus Codes
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -312,7 +312,7 @@ struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
 	inline_dentry = inline_data_addr(dir, ipage);
 
 	make_dentry_ptr_inline(dir, &d, inline_dentry);
-	de = find_target_dentry(fname, namehash, NULL, &d);
+	de = find_target_dentry(fname, namehash, NULL, &d, NULL);
 	unlock_page(ipage);
 	if (de)
 		*res_page = ipage;
