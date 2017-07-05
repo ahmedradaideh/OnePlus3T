5aae5473e95d952f677a78a090abaa28ee8de63d
Alex Naidis <alex.naidis@linux.com>
2017-07-05 14:56:30 +0200

    cpufreq: stats: Fix early return in cpufreq_stats_update_policy_cpu
    Patch cdedd5a7dafc3 ("cpufreq: stats: Fix crash if the stats table is null")
    fixed a crash however it was checking the policy->cpu table
    instead of the policy->last_cpu table.
    This commit fixes that.
    
    Change-Id: I261c61219ca01df4190a44bfb1f499a4928a263b
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -431,11 +431,13 @@ static void cpufreq_stats_update_policy_cpu(struct cpufreq_policy *policy)
 	if (stat) {
 		kfree(stat->time_in_state);
 		kfree(stat);
-	} else {
-		return;
 	}
 
 	stat = per_cpu(cpufreq_stats_table, policy->last_cpu);
+	if (!stat) {
+		return;
+	}
+
 	per_cpu(cpufreq_stats_table, policy->cpu) = per_cpu(cpufreq_stats_table,
 			policy->last_cpu);
 	per_cpu(cpufreq_stats_table, policy->last_cpu) = NULL;
