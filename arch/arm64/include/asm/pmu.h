b9043b3894d67ccc3bee8d9ea5394fc54c613d18
ahmedradaideh <ahmed.radaideh@gmail.com>
2017-07-11 18:59:04 +0300

    Squash Revert "Perf: arm64: This changes is being reverted so that it can be replaced by equivalent functionality from upstream"
    *Revert "Perf: arm64: stop counters when going into hotplug"
    This reverts commit f17a718 .
    
    *Revert "Perf: arm64: restore registers after reset"
    This reverts commit 39624f3 .
    
    *Revert "Perf: arm64: fix disable of pmu irq during hotplug"
    This reverts commit 64628ff .
    
    *Revert "Perf: arm64: support hotplug and power collapse"
    This reverts commit fbcc93e .
    
    (cherry-picked from angler kernel)
    
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- arch/arm64/include/asm/pmu.h
+++ arch/arm64/include/asm/pmu.h
@@ -21,12 +21,6 @@
 
 #ifdef CONFIG_HW_PERF_EVENTS
 
-enum arm_pmu_state {
-	ARM_PMU_STATE_OFF       = 0,
-	ARM_PMU_STATE_GOING_DOWN,
-	ARM_PMU_STATE_RUNNING,
-};
-
 /* The events for a given PMU register set. */
 struct pmu_hw_events {
 	/*
@@ -39,8 +33,6 @@ struct pmu_hw_events {
 	 * an event. A 0 means that the counter can be used.
 	 */
 	unsigned long           *used_mask;
-
-	u32			*from_idle;
 
 	/*
 	 * Hardware lock to serialize accesses to PMU registers. Needed for the
@@ -70,15 +62,11 @@ struct arm_pmu {
 	void			(*free_irq)(struct arm_pmu *);
 	int			(*map_event)(struct perf_event *event);
 	int			num_events;
-	int			pmu_state;
-	int			percpu_irq;
 	atomic_t		active_events;
 	struct mutex		reserve_mutex;
 	u64			max_period;
 	struct platform_device	*plat_device;
 	struct pmu_hw_events	*(*get_hw_events)(void);
-	void			(*save_pm_registers)(void *hcpu);
-	void			(*restore_pm_registers)(void *hcpu);
 	int			(*check_event)(
 					 struct arm_pmu *armpmu,
 					 struct hw_perf_event *hwc);
