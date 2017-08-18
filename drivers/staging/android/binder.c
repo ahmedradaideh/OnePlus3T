b8be453258b45d9a9150314f952ff77284fb1812
ahmedradaideh <ahmed.radaideh@gmail.com>
2017-08-18 22:12:44 +0300

    staging: binder: Re-add OnePlus proc block
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -70,6 +70,7 @@
 #include <linux/pid_namespace.h>
 #include <linux/security.h>
 #include <linux/spinlock.h>
+#include <linux/proc_fs.h>
 #include "binder.h"
 #include "binder_alloc.h"
 #include "binder_trace.h"
@@ -3936,6 +3937,10 @@ retry:
 		case BINDER_WORK_TRANSACTION: {
 			binder_inner_proc_unlock(proc);
 			t = container_of(w, struct binder_transaction, work);
+		if(t->from) {
+			task_thread_info(current)->pid = t->from->pid;
+			task_thread_info(current)->tgid = t->from->proc->pid;
+		}
 		} break;
 		case BINDER_WORK_RETURN_ERROR: {
 			struct binder_error *e = container_of(
@@ -5581,6 +5586,51 @@ BINDER_DEBUG_ENTRY(stats);
 BINDER_DEBUG_ENTRY(transactions);
 BINDER_DEBUG_ENTRY(transaction_log);
 
+static int proc_state_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, binder_state_show, NULL);
+}
+
+static int proc_transactions_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, binder_transactions_show, NULL);
+}
+
+static int proc_transaction_log_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, binder_transaction_log_show, &binder_transaction_log);
+}
+
+
+static const struct file_operations proc_state_operations = {
+  .open       = proc_state_open,
+  .read       = seq_read,
+  .llseek     = seq_lseek,
+  .release    = single_release,
+};
+
+static const struct file_operations proc_transactions_operations = {
+  .open       = proc_transactions_open,
+  .read       = seq_read,
+  .llseek     = seq_lseek,
+  .release    = single_release,
+};
+
+static const struct file_operations proc_transaction_log_operations = {
+  .open       = proc_transaction_log_open,
+  .read       = seq_read,
+  .llseek     = seq_lseek,
+  .release    = single_release,
+};
+
+static int binder_proc_init(void)
+{
+    proc_create("proc_state", 0, NULL, &proc_state_operations);
+    proc_create("proc_transactions", 0, NULL, &proc_transactions_operations);
+    proc_create("proc_transaction_log", 0, NULL, &proc_transaction_log_operations);
+    return 0;
+}
+
 static int __init init_binder_device(const char *name)
 {
 	int ret;
@@ -5671,7 +5721,7 @@ static int __init binder_init(void)
 		if (ret)
 			goto err_init_binder_device_failed;
 	}
-
+binder_proc_init();
 	return ret;
 
 err_init_binder_device_failed:
