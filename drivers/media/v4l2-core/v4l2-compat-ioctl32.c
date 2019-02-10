ahmedradaideh/OnePlus3T

commit 816edddbcdce065223dc8d536b5442dc30cc8e60

media: v4l2-compat-ioctl32: Re-apply 2befc16
 * Re-apply after Merging v3.18.103.

Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -385,7 +385,11 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up,
 
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up->data_offset, &up32->data_offset,
-			 sizeof(up->data_offset)))
+			 sizeof(up->data_offset)) ||
+	    copy_in_user(up->reserved, up32->reserved,
+			 sizeof(up->reserved)) ||
+	    copy_in_user(&up->length, &up32->length,
+			 sizeof(__u32)))
 		return -EFAULT;
 
 	switch (memory) {
@@ -417,7 +421,9 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up,
 
 	if (copy_in_user(up32, up, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up32->data_offset, &up->data_offset,
-			 sizeof(up->data_offset)))
+			 sizeof(up->data_offset)) ||
+	    copy_in_user(up32->reserved, up->reserved,
+			 sizeof(up32->reserved)))
 		return -EFAULT;
 
 	switch (memory) {
@@ -895,6 +901,9 @@ static int put_v4l2_ext_controls32(struct file *file,
 struct v4l2_event32 {
 	__u32				type;
 	union {
+		struct v4l2_event_vsync		vsync;
+		struct v4l2_event_ctrl		ctrl;
+		struct v4l2_event_frame_sync	frame_sync;
 		__u8			data[64];
 	} u;
 	__u32				pending;

commit f5912ab50244eb09e148b927253be38999666611

v4l2-compat-ioctl32: CleanUp Reserved code
Align the code with the marlin kernel.

Fixes: 816eddd (media: v4l2-compat-ioctl32: Re-apply 2befc16)
Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -386,10 +386,7 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up,
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up->data_offset, &up32->data_offset,
 			 sizeof(up->data_offset)) ||
-	    copy_in_user(up->reserved, up32->reserved,
-			 sizeof(up->reserved)) ||
-	    copy_in_user(&up->length, &up32->length,
-			 sizeof(__u32)))
+	    copy_in_user(up->reserved, up32->reserved, sizeof(up->reserved)))
 		return -EFAULT;
 
 	switch (memory) {
@@ -422,8 +419,7 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up,
 	if (copy_in_user(up32, up, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up32->data_offset, &up->data_offset,
 			 sizeof(up->data_offset)) ||
-	    copy_in_user(up32->reserved, up->reserved,
-			 sizeof(up32->reserved)))
+	    copy_in_user(up32->reserved, up->reserved, sizeof(up32->reserved)))
 		return -EFAULT;
 
 	switch (memory) {