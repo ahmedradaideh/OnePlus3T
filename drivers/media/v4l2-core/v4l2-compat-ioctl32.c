5f060ff89245e7e64a02b164a9a065223bf642c8
ahmedradaideh <ahmed.radaideh@gmail.com>
2019-03-26 20:58:44 +0200

    v4l2-compat-ioctl32: CleanUp Reserved code
    Align the code with the marlin kernel.
    
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -385,7 +385,8 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up,
 
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up->data_offset, &up32->data_offset,
-			 sizeof(up->data_offset)))
+			 sizeof(up->data_offset)) ||
+	    copy_in_user(up->reserved, up32->reserved, sizeof(up->reserved)))
 		return -EFAULT;
 
 	switch (memory) {
@@ -417,7 +418,8 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up,
 
 	if (copy_in_user(up32, up, 2 * sizeof(__u32)) ||
 	    copy_in_user(&up32->data_offset, &up->data_offset,
-			 sizeof(up->data_offset)))
+			 sizeof(up->data_offset)) ||
+	    copy_in_user(up32->reserved, up->reserved, sizeof(up32->reserved)))
 		return -EFAULT;
 
 	switch (memory) {
@@ -895,6 +897,9 @@ static int put_v4l2_ext_controls32(struct file *file,
 struct v4l2_event32 {
 	__u32				type;
 	union {
+		struct v4l2_event_vsync		vsync;
+		struct v4l2_event_ctrl		ctrl;
+		struct v4l2_event_frame_sync	frame_sync;
 		__u8			data[64];
 	} u;
 	__u32				pending;
