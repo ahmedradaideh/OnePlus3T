0e10e7d8bf21cd4460444bbdbfcecbbe9070468e
Ahmed Radaideh <ahmed.radaideh@gmail.com>
2019-09-29 00:46:53 +0300

    arm64: smp: Complete Merge d044c1018059
    Fixes 99d31966cb90 (Merge tag 'lsk-v3.18-17.03-android' into 'msm-3.18')
    Signed-off-by: Ahmed Radaideh <ahmed.radaideh@gmail.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -153,7 +153,6 @@ asmlinkage notrace void secondary_start_kernel(void)
 	current->active_mm = mm;
 
 	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
-	pr_debug("CPU%u: Booted secondary processor\n", cpu);
 
 	/*
 	 * TTBR0 is only used for the identity mapping at this stage. Make it
